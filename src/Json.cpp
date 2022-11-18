768557cbe184fd63d4be874216e83debb97d1255
Zhirui-Zhang <h853201808@qq.com>
2022-11-18 16:35:33 +0800

    :lock: Add Json class to avoid memory leak

--- src/Json.cpp
+++ src/Json.cpp
@@ -1,8 +1,170 @@
 #include "Json.h"
-#include "JsonValue.h"
-using namespace std;
 
 namespace myJson {
-    
+
+// define all functions declared in Json.h
+// ctor dtor cctor rvalue etc
+Json::Json() noexcept : m_jv(new JsonValue()) {}
+
+Json::Json(const Json& rhs) noexcept {
+    m_jv.reset(new JsonValue(*(rhs.m_jv)));
+}
+
+Json& Json::operator=(const Json& rhs) noexcept {
+    m_jv.reset(new JsonValue(*(rhs.m_jv)));
+    return *this;
+}
+
+Json::Json(Json&& rhs) noexcept {
+    m_jv.reset(rhs.m_jv.release());
+}
+
+Json& Json::operator=(Json&& rhs) noexcept {
+    m_jv.reset(rhs.m_jv.release());
+    return *this;
+}
+
+// parse/stringify function
+int Json::parse(const string& json) noexcept {
+    int res = m_jv->parse(json);
+    return res;
+}
+
+void Json::stringify(string& str) const noexcept {
+    m_jv->stringify(str);
+}
+
+// copy move swap function
+void Json::copy(const Json& rhs) noexcept {
+    m_jv.reset(new JsonValue(*(rhs.m_jv)));
+}
+
+void Json::move(Json& rhs) noexcept {
+    m_jv.reset(new JsonValue(*(rhs.m_jv)));
+    // after moving action was finished, call JsonValue dtor in rhs.m_jv to assure JsonValue was moved successfully
+    // but not unique_ptr release function, coz it will destory rhs object, causing v2(in JsonTest.cpp) not available
+    rhs.m_jv->~JsonValue();
+}
+
+void Json::swap(Json& rhs) noexcept {
+    // add std here, or it will call member function first
+    std::swap(m_jv, rhs.m_jv);
+}
+
+// all kinds of API provided for user 
+JSON_TYPE Json::get_type() const noexcept {
+    return m_jv->get_type();
+}
+
+void Json::set_type(JSON_TYPE type) noexcept {
+    m_jv->set_type(type);
+}
+
+double Json::get_number() const noexcept {
+    return m_jv->get_number();
+}
+
+void Json::set_number(double d) noexcept {
+    m_jv->set_number(d);
+}
+
+const string& Json::get_string() const noexcept {
+    return m_jv->get_string();
+}   
+
+size_t Json::get_string_length() const noexcept {
+    return m_jv->get_string_length();
+}
+
+void Json::set_string(const string& str) noexcept {
+    m_jv->set_string(str);
+}
+
+void Json::set_array() noexcept {
+    // use tmp object as actual parameter to construct array
+    m_jv->set_array(vector<JsonValue>());
+}
+
+size_t Json::get_array_size() const noexcept {
+    return m_jv->get_array_size();
+}
+
+size_t Json::get_array_capacity() const noexcept {
+    return m_jv->get_array_capacity();
+}
+
+void Json::reserve_array(size_t capacity) noexcept {
+    m_jv->reserve_array(capacity);
+}
+
+void Json::shrink_array() noexcept {
+    m_jv->shrink_array();
+}
+
+void Json::clear_array() noexcept {
+    m_jv->clear_array();
+}
+
+const Json Json::get_array_element(size_t index) const noexcept {
+    // create new Json object ptr to return JsonValue object, use const Json as return value to avoid local variable problem 
+    Json res;
+    res.m_jv.reset(new JsonValue(m_jv->get_array_element(index)));
+    return res;
+}
+
+void Json::pushback_array_element(const Json& jv) noexcept {
+    m_jv->pushback_array_element(*jv.m_jv);
+}
+
+void Json::popback_array_element() noexcept {
+    m_jv->popback_array_element();
+}
+
+void Json::insert_array_element(size_t index, const Json& j) noexcept{
+    m_jv->insert_array_element(index, *j.m_jv);
+}
+
+void Json::erase_array_element(size_t index, size_t count) noexcept {
+    m_jv->erase_array_element(index, count);
+}
+
+size_t Json::get_object_size() const noexcept {
+    return m_jv->get_object_size();
+}
+
+// using existed fuction in std::map
+void Json::set_object() noexcept {
+    m_jv->set_object(map<string, JsonValue>());
+}
+
+void Json::clear_object() noexcept {
+    m_jv->clear_object();
+}
+
+bool Json::find_object_key(const string& key) const noexcept {
+    return m_jv->find_object_key(key);
+}
+
+const Json Json::get_object_value(const string& key) const noexcept {
+    Json res;
+    res.m_jv.reset(new JsonValue(m_jv->get_object_value(key)));
+    return res;
+}
+
+void Json::set_object_value(const string& key, const Json& val) noexcept {
+    m_jv->set_object_value(key, *val.m_jv);
+}
+
+void Json::remove_object_value(const string& key) noexcept {
+    m_jv->remove_object_value(key);
+}
+
+bool operator==(const Json& lhs, const Json& rhs) noexcept {
+    return *lhs.m_jv == *rhs.m_jv;
+}
+
+bool operator!=(const Json& lhs, const Json& rhs) noexcept {
+    return !(lhs == rhs);
+}
 
 };
--- src/Json.h
+++ src/Json.h
@@ -1,86 +1,79 @@
 #ifndef JSON_H
 #define JSON_H
 #include <string>
-#include <cassert>
 #include <memory>   // unique_ptr
 #include <cstddef>  // size_t
+#include "JsonEnum.h"
+#include "JsonValue.h"
 using namespace std;
 
 namespace myJson {
 
-// forward declaration
-class JsonValue;    
+// forward delclaration
+class JsonValue;
 
 class Json {
 public:
     // ctor dtor cctor rvalue etc
     Json() noexcept;
-    ~Json() noexcept;
+    ~Json() noexcept {}
     Json(const Json& rhs) noexcept;
     Json& operator=(const Json& rhs) noexcept;
     Json(Json&& rhs) noexcept;
     Json& operator=(Json&& rhs) noexcept;
 
-    // override for == != operator
-    bool operator==(const Json& rhs) noexcept;
-    bool operator!=(const Json& rhs) noexcept;
+    // parse/stringify function
+    int parse(const string& json) noexcept;
+    void stringify(string& str) const noexcept;
 
     // copy move swap function
-    void json_copy(const Json& rhs) noexcept;
-    void json_move(Json& rhs) noexcept;
-    void json_swap(Json& rhs) noexcept;
-
-    // parse/stringify function
-    void json_parse(const string& json) noexcept;
-    void json_parse_file(const string& filename) noexcept;
-    void json_stringify(string& str) const noexcept;
-
+    void copy(const Json& rhs) noexcept;
+    void move(Json& rhs) noexcept;
+    void swap(Json& rhs) noexcept;
+    
     // all kinds of API provided for user 
-    JSON_TYPE json_get_type() const noexcept;
+    JSON_TYPE get_type() const noexcept;
+    void set_type(JSON_TYPE type) noexcept;
 
-    void json_set_null() noexcept;
+    double get_number() const noexcept;
+    void set_number(double n) noexcept;
 
-    int json_get_boolean() const noexcept;
-    void json_set_boolean(bool b) noexcept;
+    const string& get_string() const noexcept;
+    size_t get_string_length() const noexcept;
+    void set_string(const string& str) noexcept;
 
-    double json_get_number() const noexcept;
-    void json_set_number(double n) noexcept;
+    void set_array() noexcept;
+    size_t get_array_size() const noexcept;
+    size_t get_array_capacity() const noexcept;
+    void reserve_array(size_t capacity) noexcept;
+    void shrink_array() noexcept;
+    void clear_array() noexcept;
+    const Json get_array_element(size_t index) const noexcept;
+    void pushback_array_element(const Json& j) noexcept;
+    void popback_array_element() noexcept;
+    void insert_array_element(size_t index, const Json& j) noexcept;
+    void erase_array_element(size_t index, size_t count) noexcept;
 
-    const string json_get_string() const noexcept;
-    size_t json_get_string_length() const noexcept;
-    void json_set_string(const string& str) noexcept;
-
-    void json_set_array(size_t capacity) noexcept;
-    size_t json_get_array_size() const noexcept;
-    size_t json_get_array_capacity() const noexcept;
-    void json_reserve_array(size_t capacity) noexcept;
-    void json_shrink_array() noexcept;
-    void json_clear_array() noexcept;
-    Json json_get_array_element(size_t index) const noexcept;
-    Json json_pushback_array_element(const Json& j) noexcept;
-    void json_popback_array_element() noexcept;
-    Json json_insert_array_element(const Json& j, size_t index);
-    void json_erase_array_element(size_t index, size_t count) noexcept;
-
-    void json_set_object(size_t capacity) noexcept;
-    size_t json_get_object_size() const noexcept;
-    size_t json_get_object_capacity() const noexcept;
-    void json_reserve_object(size_t capacity) noexcept;
-    void json_shrink_object() noexcept;
-    void json_clear_object() noexcept;
-    const string& json_get_object_key(size_t index) const noexcept;
-    size_t json_get_object_key_length(size_t index) const noexcept;
-    Json json_get_object_value(size_t index) const noexcept;
-    size_t json_find_object_index(const string& key) const noexcept;
-    Json json_find_object_value(const string& key) const noexcept;
-    Json json_set_object_value(const string& key, const Json& val) noexcept;
-    void json_remove_object_value(size_t index) noexcept;
+    void set_object() noexcept;
+    size_t get_object_size() const noexcept;
+    void clear_object() noexcept;
+    bool find_object_key(const string& key) const noexcept;
+    const Json get_object_value(const string& key) const noexcept;
+    void set_object_value(const string& key, const Json& val) noexcept;
+    void remove_object_value(const string& key) noexcept;
 
 private:
-    // create an small ptr to JsonValue class, Json class provides API while JsonValue class takes charge of realization
-    // unique ptr has a counter inside and release automatically when counter equals to 0, so there is no init/free function
-    unique_ptr<myJson::JsonValue> m_jv;
+    // create an smart ptr to JsonValue class, Json class provides API while JsonValue class takes charge of realization
+    // unique ptr has a counter inside and release automatically when counter equals to 0, so all members can be destroyed without memory leak
+    unique_ptr<JsonValue> m_jv;
+
+    // override for == and != operator
+    friend bool operator==(const Json& lhs, const Json& rhs) noexcept;
+    friend bool operator!=(const Json& lhs, const Json& rhs) noexcept;
 };
+
+bool operator==(const Json& lhs, const Json& rhs) noexcept;
+bool operator!=(const Json& lhs, const Json& rhs) noexcept;
 
 };
 
